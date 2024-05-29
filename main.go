64b7cb2954d85e5ec425d2b341ce916fdc067a35
alekseinovikov <novikov.aleksei.92@gmail.com>
2024-05-29 14:03:37 +0300

    Init

--- main.go
+++ main.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net"
+	"strconv"
+)
 
+var address = "habr.com"
+var ports = []int{80, 443}
+
+func handleConnection(conn net.Conn, port int) {
+	destination, err := net.Dial("tcp", address+":"+strconv.Itoa(port))
+	if err != nil {
+		log.Println("Error connecting to destination:", err)
+		conn.Close()
+		return
+	}
+	defer destination.Close()
+
+	go func() {
+		_, err := io.Copy(destination, conn)
+		if err != nil {
+			log.Println("Error copying from client to destination:", err)
+		}
+	}()
+
+	_, err = io.Copy(conn, destination)
+	if err != nil {
+		log.Println("Error copying from destination to client:", err)
+	}
+}
+
+func mdain() {
+	for _, port := range ports {
+		go func(port int) {
+			listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+			if err != nil {
+				log.Fatal("Error starting listener:", err)
+			}
+			defer listener.Close()
+
+			log.Printf("Listening on port %d\n", port)
+
+			for {
+				conn, err := listener.Accept()
+				if err != nil {
+					log.Println("Error accepting connection:", err)
+					continue
+				}
+
+				go handleConnection(conn, port)
+			}
+		}(port)
+	}
+
+	select {}
 }
